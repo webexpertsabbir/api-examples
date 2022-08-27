7d69b5983d1f9ff8e781078e0ad8ca0b7ce60347
Sabbir <webexpertsabbir@gmail.com>
2022-08-27 23:15:55 +0600

    random user

---
+++ js/randomUser.js
@@ -0,0 +1,19 @@
+const loadUser = () =>{
+    fetch('https://randomuser.me/api/?results=50')
+    .then(res => res.json())
+    .then(data => dispalyUser(data.results))
+}
+const dispalyUser = users => {
+ const userContainer = document.getElementById('user-container');
+ for(const user of users){
+    console.log(user);
+    const userDiv = document.createElement('div');
+    userDiv.innerHTML = `
+    <h3>User Name: ${user.name.first} ${user.name.last}</h3>
+    <p>Email: ${user.email}</p>
+    <p>Gender: ${user.gender} Age: ${user.dob.age}</p>
+    `;
+    userContainer.appendChild(userDiv);
+ }
+}
+loadUser();
